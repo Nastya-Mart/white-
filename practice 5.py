50991561edf9cad0411924364d10fb34de28be5e
Nastya-Mart <nastya.mart749@gmail.com>
2024-10-13 17:30:16 +0300

    Add files via upload

---
+++ practice 5.py
@@ -0,0 +1,55 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+index = 1
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+
+index = 2
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+
+index = 3
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+
+index = 4
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+
+index = 5
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
+
+index = 6
+elem = my_list[index]
+while elem >= 0 and index < len(my_list):
+    if my_list[index] > 0:
+        print(elem)
+    break
+
